14ca83b7b5b6ffaacfba3f849ea21118481a2b1c
Roberto Sartori <roberto.sartori@gmail.com>
2019-11-11 01:19:56 +0100

    instance icon added

--- CorrectMagentaStars.js
+++ CorrectMagentaStars.js
@@ -44,8 +44,9 @@
 // POSSIBILITY OF SUCH DAMAGE.
 // ****************************************************************************
 
+/* beautify ignore:start */
 #define TITLE "CorrectMagentaStars"
-#define VERSION "1.0"
+#define VERSION "1.1"
 
 #feature-id Utilities > CorrectMagentaStars
 
@@ -63,231 +64,295 @@
 #include <pjsr/SampleType.jsh>
 #include <pjsr/Sizer.jsh>
 #include <pjsr/UndoFlag.jsh>
+/* beautify ignore:end */
+
+
 
 // The script's parameters prototype.
-function parametersPrototype() {
-    this.targetView = null;
-    this.scnrAmount = 0.5;
+function parametersPrototype()
+{
+   this.setDefaults = function()
+   {
+      this.targetView = null;
+      this.scnrAmount = 0.8;
+   }
 
-    this.setParameters = function () {
-        Parameters.clear();
-        Parameters.set("scnrAmount", this.scnrAmount);
-    }
+   this.setParameters = function()
+   {
+      Parameters.clear();
+      Parameters.set( "scnrAmount", this.scnrAmount );
+   }
 
-    this.getParameters = function () {
-        this.scnrAmount = Parameters.has("scnrAmount") ? Parameters.getReal("scnrAmount") : 0.5;
-    }
+   this.getParameters = function()
+   {
+      if ( Parameters.has( "scnrAmount" ) )
+      {
+         this.scnrAmount = Parameters.getReal( "scnrAmount" );
+      }
+   }
 }
 var parameters = new parametersPrototype();
+parameters.setDefaults();
+parameters.getParameters();
 
 // Returns a push button with given text and onClick function.
-function pushButtonWithTextOnClick(parent, text_, onClick_) {
-    var button = new PushButton(parent);
+function pushButtonWithTextOnClick( parent, text_, onClick_ )
+{
+   var button = new PushButton( parent );
 
-    button.text = text_;
-    button.onClick = onClick_;
+   button.text = text_;
+   button.onClick = onClick_;
 
-    return button;
+   return button;
 }
 
 // The script's parameters dialog prototype.
-function parametersDialogPrototype() {
-    this.__base__ = Dialog;
-    this.__base__();
+function parametersDialogPrototype()
+{
+   this.__base__ = Dialog;
+   this.__base__();
 
-    var labelMinWidth = Math.round(this.font.width("Amount:") + 2.0 * this.font.width('M'));
+   var labelMinWidth = Math.round( this.font.width( "Amount:" ) + 2.0 * this.font.width( 'M' ) );
 
-    var sliderMaxValue = 256;
-    var sliderMinWidth = 256;
+   var sliderMaxValue = 256;
+   var sliderMinWidth = 256;
 
-    this.windowTitle = TITLE;
+   this.windowTitle = TITLE;
 
-    this.titlePane = new Label(this);
+   this.titlePane = new Label( this );
 
-    this.titlePane.frameStyle = FrameStyle_Box;
-    this.titlePane.margin = 4;
-    this.titlePane.wordWrapping = true;
-    this.titlePane.useRichText = true;
-    this.titlePane.text =
-        "<p><b>" + TITLE + " Version " + VERSION + "</b> &mdash; " +
-        "This script reduces the residual magenta chromatism affecting stars that can be present " +
-        "after combining Hydrogen-Alpha, Sulphur-II and Oxygen-III channels using the Hubble Palette. " +
-        "<p>Copyright &copy; 2012-2015 Roberto Sartori, Edoardo Luca Radice. All Rights Reserved.</p>";
+   this.titlePane.frameStyle = FrameStyle_Box;
+   this.titlePane.margin = 4;
+   this.titlePane.wordWrapping = true;
+   this.titlePane.useRichText = true;
+   this.titlePane.text =
+      "<p><b>" + TITLE + " Version " + VERSION + "</b> &mdash; " +
+      "This script reduces the residual magenta chromatism affecting stars that can be present " +
+      "after combining Hydrogen-Alpha, Sulphur-II and Oxygen-III channels using the Hubble Palette. " +
+      "<p>Copyright &copy; 2012-2015 Roberto Sartori, Edoardo Luca Radice. All Rights Reserved.</p>";
 
-    this.targetView = new HorizontalSizer;
+   this.targetView = new HorizontalSizer;
 
-    this.viewList = new ViewList(this);
-    this.viewListNullCurrentView = this.viewList.currentView;
+   this.viewList = new ViewList( this );
+   this.viewListNullCurrentView = this.viewList.currentView;
 
-    this.viewList.getAll();
-    if (parameters.targetView !== null && parameters.targetView.isView) {
-        this.viewList.currentView = parameters.targetView;
-    }
-    else {
-        parameters.targetView = this.viewList.currentView;
-    }
-    this.viewList.onViewSelected = function (view) { parameters.targetView = view; }
+   this.viewList.getAll();
+   if ( parameters.targetView !== null && parameters.targetView.isView )
+   {
+      this.viewList.currentView = parameters.targetView;
+   }
+   else
+   {
+      parameters.targetView = this.viewList.currentView;
+   }
+   this.viewList.onViewSelected = function( view )
+   {
+      parameters.targetView = view;
+   }
 
-    this.targetView.add(this.viewList);
+   this.targetView.add( this.viewList );
 
-    this.parameterPane = new VerticalSizer;
+   this.parameterPane = new VerticalSizer;
 
-    this.parameterPane.margin = 6;
-    this.parameterPane.spacing = 4;
+   this.parameterPane.margin = 6;
+   this.parameterPane.spacing = 4;
 
-    this.scnrAmountControl = new NumericControl(this);
+   this.scnrAmountControl = new NumericControl( this );
 
-    this.scnrAmountControl.label.text = "Amount:";
-    this.scnrAmountControl.label.minWidth = labelMinWidth;
-    this.scnrAmountControl.slider.setRange(0, sliderMaxValue);
-    this.scnrAmountControl.slider.minWidth = sliderMinWidth;
-    this.scnrAmountControl.setRange(0.0, 1.0);
-    this.scnrAmountControl.setPrecision(2);
-    this.scnrAmountControl.setValue(parameters.scnrAmount);
-    this.scnrAmountControl.onValueUpdated = function (value) { parameters.scnrAmount = value; }
-    this.scnrAmountControl.toolTip =
-        "<p>SCNR amount applied to the inverted image.</p>";
+   this.scnrAmountControl.label.text = "Amount:";
+   this.scnrAmountControl.label.minWidth = labelMinWidth;
+   this.scnrAmountControl.slider.setRange( 0, sliderMaxValue );
+   this.scnrAmountControl.slider.minWidth = sliderMinWidth;
+   this.scnrAmountControl.setRange( 0.0, 1.0 );
+   this.scnrAmountControl.setPrecision( 2 );
+   this.scnrAmountControl.setValue( parameters.scnrAmount );
+   this.scnrAmountControl.onValueUpdated = function( value )
+   {
+      parameters.scnrAmount = value;
+   }
+   this.scnrAmountControl.toolTip =
+      "<p>SCNR amount applied to the inverted image.</p>";
 
-    this.parameterPane.add(this.scnrAmountControl);
+   this.parameterPane.add( this.scnrAmountControl );
 
-    this.buttonPane = new HorizontalSizer;
+   this.buttonPane = new HorizontalSizer;
 
-    this.buttonPane.spacing = 6;
+   this.buttonPane.spacing = 6;
 
-    this.buttonPane.addStretch();
-    this.buttonPane.add(pushButtonWithTextOnClick(this, "Execute", function () {
-        parameters.exit = false;
-        this.dialog.ok();
-    }));
-    this.buttonPane.add(pushButtonWithTextOnClick(this, "Close", function () {
-        parameters.exit = true;
-        this.dialog.ok();
-    }));
+   this.buttonPane.addStretch();
+   this.buttonPane.add( pushButtonWithTextOnClick( this, "Execute", function()
+   {
+      parameters.exit = false;
+      this.dialog.ok();
+   } ) );
+   this.buttonPane.add( pushButtonWithTextOnClick( this, "Close", function()
+   {
+      parameters.exit = true;
+      this.dialog.ok();
+   } ) );
+   this.bottomBarPane = new HorizontalSizer;
 
-    this.sizer = new VerticalSizer;
+   this.newInstanceButton = new ToolButton( this );
+   this.newInstanceButton.icon = new Bitmap( ":/images/interface/dragObject.png" );
+   this.newInstanceButton.setScaledFixedSize( 20, 20 );
+   this.newInstanceButton.toolTip = "New Instance";
+   this.newInstanceButton.onMousePress = function()
+   {
+      this.hasFocus = true;
+      this.pushed = false;
+      parameters.setParameters();
+      this.dialog.newInstance();
+   };
 
-    this.sizer.margin = 6;
-    this.sizer.spacing = 6;
-    this.sizer.add(this.titlePane);
-    this.sizer.add(this.targetView);
-    this.sizer.add(this.parameterPane);
-    this.sizer.add(this.buttonPane);
+   this.bottomBarPane.add( this.newInstanceButton );
+   this.bottomBarPane.add( this.buttonPane );
+   this.sizer = new VerticalSizer;
 
-    this.adjustToContents();
-    this.setFixedSize();
+   this.sizer.margin = 6;
+   this.sizer.spacing = 6;
+   this.sizer.add( this.titlePane );
+   this.sizer.add( this.targetView );
+   this.sizer.add( this.parameterPane );
+   this.sizer.add( this.bottomBarPane );
+
+   this.adjustToContents();
+   this.setFixedSize();
 }
 parametersDialogPrototype.prototype = new Dialog;
 
 // The script's process prototype.
-function processPrototype() {
+function processPrototype()
+{
 
-    this.writeParameters = function (parameters) {
-        console.writeln("Target view: ", parameters.targetView.fullId);
-        console.writeln("Amount: ", format("%d", parameters.scnrAmount));
-    }
+   this.executeMagentaCorrection = function( targetView )
+   {
+      var PInvert = new Invert;
+      var PSCNR = new SCNR;
+      PSCNR.amount = Math.max( 0, Math.min( 1, parameters.scnrAmount ) );
+      PSCNR.protectionMethod = SCNR.prototype.AverageNeutral;
+      PSCNR.colorToRemove = SCNR.prototype.Green;
+      PSCNR.preserveLightness = true;
 
-    this.createImageWindowOfView = function (view) {
-        var imageWindow = createImageWindow(
-            view.image.width, view.image.height, uniqueViewId(mainViewIdOfView(view)), view.image
-        );
+      var P = new ProcessContainer;
 
-        return imageWindow;
-    }
+      P.add( PInvert );
+      P.add( PSCNR );
+      P.add( PInvert );
+      P.executeOn( targetView, true );
+   }
 
-    this.executeMagentaCorrection = function (targetView) {
-        var PInvert = new Invert;
-        var PSCNR = new SCNR;
-        PSCNR.amount = parameters.scnrAmount;
-        PSCNR.protectionMethod = SCNR.prototype.AverageNeutral;
-        PSCNR.colorToRemove = SCNR.prototype.Green;
-        PSCNR.preserveLightness = true;
-        
-        var P = new ProcessContainer;
-
-        P.add(PInvert);
-        P.add(PSCNR);
-        P.add(PInvert);
-        P.executeOn(targetView, true);
-    }
-
-    this.execute = function () {
-        this.executeMagentaCorrection(parameters.targetView);
-    }
+   this.execute = function()
+   {
+      this.executeMagentaCorrection( parameters.targetView );
+   }
 }
 var process = new processPrototype();
 
-function colorspaceIsValid(image) {
-    // An active window must exist
-    if (image.isGrayscale) {
-        (new MessageBox(
-            "<p>Error: Target view color space cannot be Grayscale.</p>",
-            TITLE,
-            StdIcon_Warning,
-            StdButton_Ok
-        )).execute();
-        return false;
-    }
-    return true;
+function colorspaceIsValid( image )
+{
+   // grayscale is not a valid colorspace for this script
+   return !image.isGrayscale
 }
 
-function performChecksAndExecute() {
-    if (!parameters.targetView ||
-        !parameters.targetView.image ||
-        !colorspaceIsValid(parameters.targetView.image)) {
-        return;
-    }
+function performChecksAndExecute()
+{
+   console.noteln( "Applying CorrectMagentaStars on target ", ImageWindow.activeWindow.currentView.id );
 
-    process.execute();
+   if ( !parameters.targetView ||
+      !parameters.targetView.image )
+   {
+      ( new MessageBox(
+         "<p>Correct Magenta Stars Error:<br><br>Undefined target view.</p>",
+         TITLE,
+         StdIcon_Warning,
+         StdButton_Ok
+      ) ).execute();
+      return;
+   }
+   if ( !colorspaceIsValid( parameters.targetView.image ) )
+   {
+      ( new MessageBox(
+         "<p>Correct Magenta Stars Error:<br><br>Target view color space cannot be Grayscale.</p>",
+         TITLE,
+         StdIcon_Warning,
+         StdButton_Ok
+      ) ).execute();
+      return;
+   }
+
+   process.execute();
 }
 
-function executeInGlobalContext() {
-    parameters.targetView = ImageWindow.activeWindow.currentView;
-    parameters.getParameters();
-    performChecksAndExecute();
+function executeInGlobalContext()
+{
+   parameters.targetView = ImageWindow.activeWindow.currentView;
+   parameters.getParameters();
+   performChecksAndExecute();
 }
 
-function main() {
-    console.hide();
-    
-    if (Parameters.isGlobalTarget) {
-        // Script has been launched in global context, execute and exit
-        executeInGlobalContext();
-        return;
-    }
+function executeOnTargetView( view )
+{
+   parameters.targetView = view;
+   parameters.getParameters();
+   performChecksAndExecute();
+}
 
-    // Prepare the dialog
-    var parametersDialog = new parametersDialogPrototype();
-    parameters.exit = false;
+function main()
+{
+   console.hide();
 
-    // Runloop
-    while (!parameters.exit) {
-        
-        if (Parameters.isViewTarget && !parameters.targetView) {
-            // A target is already defined, init it as the target view
-            parameters.targetView = this.targetView;
-            parameters.getParameters();
-        }
-        else {
-            // Dialog needs to be opened in order to select the image and set parameters
-            // Use the current active view as target by default
-            parameters.targetView = ImageWindow.activeWindow.currentView;            
-        }
+   if ( Parameters.isGlobalTarget )
+   {
+      // Script has been launched in global context, execute and exit
+      executeInGlobalContext();
+      return;
+   }
 
-        // Run the dalog
-        var parametersDialog = new parametersDialogPrototype();
-        if (!parametersDialog.execute()) {
-            // Dialog closure forced
-            return;
-        }
+   if ( Parameters.isViewTarget )
+   {
+      // Script has been launched on a view target, execute and exit
+      executeOnTargetView( Parameters.targetView );
+      return;
+   }
 
-        // do the job
-        if (!parameters.exit) {
-            performChecksAndExecute();            
-        }
+   // Prepare the dialog
+   var parametersDialog = new parametersDialogPrototype();
+   parameters.exit = false;
 
-        // Workaround to avoid image window close crash in 1.8 RC7.
-        parametersDialog.viewList.currentView = parametersDialog.viewListNullCurrentView;
-    }
+   // Runloop
+   while ( !parameters.exit )
+   {
+
+      if ( Parameters.isViewTarget && !parameters.targetView )
+      {
+         // A target is already defined, init it as the target view
+         parameters.targetView = this.targetView;
+         parameters.getParameters();
+      }
+      else
+      {
+         // Dialog needs to be opened in order to select the image and set parameters
+         // Use the current active view as target by default
+         parameters.targetView = ImageWindow.activeWindow.currentView;
+      }
+
+      // Run the dalog
+      var parametersDialog = new parametersDialogPrototype();
+      if ( !parametersDialog.execute() )
+      {
+         // Dialog closure forced
+         return;
+      }
+
+      // do the job
+      if ( !parameters.exit )
+      {
+         performChecksAndExecute();
+      }
+
+      // Workaround to avoid image window close crash in 1.8 RC7.
+      parametersDialog.viewList.currentView = parametersDialog.viewListNullCurrentView;
+   }
 }
 
 main();
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # correctMagentaStars
-A PixInsight script to remove magenta halo surrounding stars in Hubble Palette color composition. A collaboration with Edoardo Luca Radice.
+A PixInsight script to remove magenta halo surrounding stars in Hubble Palette color composition. In collaboration with Edoardo Luca Radice.
